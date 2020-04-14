b3fd3c7af017a1e1bb6d101a4e2d24810588037f
Jorge Contreras <jorge.contreras789@gmail.com>
2020-04-13 20:39:36 -0400

    Added traversing and transforming the AST

--- tests/traverse.test.js
+++ tests/traverse.test.js
@@ -1,7 +1,7 @@
 const { traverse } = require('../src/traverse');
 
 describe(traverse, () => {
-  it.skip('should travel to all the nodes in the tree and reverse the math', () => {
+  it('should travel to all the nodes in the tree and reverse the math', () => {
     const ast = {
       type: 'CallExpression',
       name: 'add',
@@ -31,7 +31,7 @@ describe(traverse, () => {
     expect(ast.name).toBe('subtract');
   });
 
-  it.skip('should travel to all the nodes in the tree and double all of the numbers', () => {
+  it('should travel to all the nodes in the tree and double all of the numbers', () => {
     const ast = {
       type: 'CallExpression',
       name: 'add',
@@ -53,5 +53,30 @@ describe(traverse, () => {
 
     expect(ast.arguments[0].value).toBe(24);
     expect(ast.arguments[1].value).toBe(12);
+  });
+
+  it('should modify the tree', () => {
+    const ast = {
+      type: 'CallExpression',
+      name: '+',
+      arguments: [
+        { type: 'NumericLiteral', value: 12 },
+        { type: 'NumericLiteral', value: 6 },
+      ],
+    };
+
+    const visitor = {
+      CallExpression: {
+        enter({ node }) {
+          if (node.name === '+') {
+            node.name = 'add';
+          }
+        },
+      },
+    };
+
+    traverse(ast, visitor);
+
+    expect(ast.name).toBe('add');
   });
 });
